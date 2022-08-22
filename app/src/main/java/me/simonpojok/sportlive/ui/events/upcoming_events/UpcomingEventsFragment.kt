fdd8b3e031d600da00cb26eef078df472d6ec5a9
simonpojok <simonpojok@gmail.com>
2022-08-22 11:16:07 +0300

    finished the screen logic

--- app/src/main/java/me/simonpojok/sportlive/ui/events/upcoming_events/UpcomingEventsFragment.kt
+++ app/src/main/java/me/simonpojok/sportlive/ui/events/upcoming_events/UpcomingEventsFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import me.simonpojok.sportlive.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class UpcomingEventsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -23,7 +23,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(UpcomingEventsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
